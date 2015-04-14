10c55ad5137ee36b0aff84ff9148918315eb004b
Kevin NIemiller <niemilkm@gmail.com>
2015-04-14 18:34:02 -0400

    Initial Comitt

---
+++ README.md
@@ -0,0 +1,7 @@
+# MNS-Boilerplate
+For all the Meteor Night Schoolers out there who want to get coding!
+
+### Key included packages
+ - iron:router - https://github.com/iron-meteor/iron-router/blob/devel/Guide.md
+ - alanning:roles - https://github.com/alanning/meteor-roles
+ - cunneen:mailgun - Email
---
+++ client/templates/home/home.js
@@ -0,0 +1,20 @@
+Router.route('/', function () {
+  this.render('home');
+});
+
+
+// counter starts at 0
+Session.setDefault('counter', 0);
+
+Template.home.helpers({
+  counter: function () {
+    return Session.get('counter');
+  }
+});
+
+Template.home.events({
+  'click button': function () {
+    // increment the counter when button is clicked
+    Session.set('counter', Session.get('counter') + 1);
+  }
+});
